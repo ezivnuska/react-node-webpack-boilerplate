6b64b38b693d19c1c6454d869fdeed25f798f0fa
Eric Zivnuska <ezivnuska@gmail.com>
2020-09-19 09:12:54 -0700

    added button click action in MyFunctionalComponent

--- src/components/MyFunctionalComponent/index.js
+++ src/components/MyFunctionalComponent/index.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 import './MyFunctionalComponent.scss'
 
-const MyFunctionalComponent = props => (
-    <div className='my-functional-component'>MyFunctionalComponent</div>
-)
+const MyFunctionalComponent = props => {
+    
+    const onButtonClick = e => {
+        e.preventDefault()
+        alert('button clicked!')
+    }
+    
+    return (
+        <div
+            className='my-functional-component'
+            onClick={e => onButtonClick(e)}
+        >
+            MyFunctionalComponent
+        </div>
+    )
+}
 
 export default MyFunctionalComponent
