2a186e55be9e68a53e04714a1b7225e69ed011d1
Eric Zivnuska <ezivnuska@gmail.com>
2020-09-19 10:05:02 -0700

    added description to Layout content

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -11,6 +11,16 @@ const Layout = props => (
     <div id='layout'>
         <Header title="Zach&rsquo;s Site" />
         <div id='main-content'>
+            <p>These two components are, basically, the same.</p>
+            <p>
+                The difference is that
+                <br />
+                one is a functional component (a function)
+                <br />
+                and the other is a class component (React)
+                <br />
+                that uses built-in lifecycle events.
+            </p>
             <MyComponent />
             <MyFunctionalComponent />
         </div>
